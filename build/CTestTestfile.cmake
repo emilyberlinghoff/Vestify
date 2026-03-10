f0891bb4fb4f573dd41ebfc8ff694f8486aad633
Maxime Lavoie <mlavoie8@uwo.ca>
2026-03-10 06:31:21 -0400

    Remove build artifacts and virtual environment

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxim/3307/group13/Vestify
-# Build directory: /home/maxim/3307/group13/Vestify/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/nlohmann_json-build")
-subdirs("tests")
